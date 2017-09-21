c9e18f143e134398cf063d96838e354f718c687b
Anders Asplund <anders@Anderss-MacBook-Pro.local>
2017-09-21 15:19:06 +0200

    move cert to thing

--- dbsetup.sql
+++
@@ -1,9 +0,0 @@
-create table stores (
-	id serial primary key,
-    name varchar(64) not null
-);
-create table hardwares (
-	id serial primary key,
-    deviceId varchar(64) not null,
-    fk_store integer REFERENCES stores (id)
-)
